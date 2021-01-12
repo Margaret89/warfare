49fc5f512fe0d3a78c591fd860c88f85b413cdfa
Margaret <match14@yandex.ru>
2021-01-12 22:23:37 +0300

    screen 1

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,17 @@
 import {$} from './common';
 
-$(window).scroll(function(){
-	if($(this).scrollTop()>300){
-		$('.js-move-up').addClass('visible');
-	}else{
-		$('.js-move-up').removeClass('visible');
-	}
+$('.js-lang-main').on('click', function() {
+	$(this).parent('.js-lang').toggleClass('open');
+	$(this).siblings('.js-lang-popup').slideToggle(300);
 });
-$('.js-move-up').click(function(){$('body,html').animate({scrollTop:0},800);return false;});
+
+$('.js-lang-item').on('click', function() {
+	var curImg = $(this).find('.js-lang-img').attr('src');
+	var curText = $(this).find('.js-lang-text').text();
+
+	$('.js-lang-main').find('.js-lang-img').attr('src', curImg);
+	$('.js-lang-main').find('.js-lang-text').text(curText);
+
+	$(this).parents('.js-lang').removeClass('open');
+	$(this).parent('.js-lang-popup').slideUp(300);
+});
