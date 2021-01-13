576cd5264d7af42de172edd0b4ee488e08e999fa
match14 <match14@yandex.ru>
2021-01-13 12:01:59 +0300

    added styles

--- src/js/index.js
+++ src/js/index.js
@@ -22,3 +22,16 @@ $('.js-slider-tanks').slick({
 	nextArrow:'<div class="slick-next slick-arrow"><svg class="icon ic-arrow-right" width="26" height="40"><use xlink:href="assets/sprites/sprite.svg#ic-arrow-right"></use></svg></div>',
 	prevArrow:'<div class="slick-prev slick-arrow"><svg class="icon ic-arrow-left" width="26" height="40"><use xlink:href="assets/sprites/sprite.svg#ic-arrow-left"></use></svg></div>'
 });
+
+// Переключение услуг
+$('.js-services-list-decs-item:first-child').fadeIn();
+
+$('.js-services-list-item').on('click', function(){
+	var index = $(this).data('index');
+
+	$('.js-services-list-item').removeClass('active');
+	$(this).addClass('active');
+
+	$('.js-services-list-decs-item').css('display', 'none');
+	$('.js-services-list-decs-item[data-index="'+index+'"]').fadeIn(1000);
+});
